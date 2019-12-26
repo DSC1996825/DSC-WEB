2ce083b500c68b2bfd1e3e9a83bede96f674f6e1
DSC1996825 <1049717267@qq.com>
2019-12-26 17:28:19 +0800

    创建项目
    Signed-off-by: DSC1996825 <1049717267@qq.com>

---
+++ src/pages/user/list/index.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import List from './List';
+export default class UserList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <List />
+        666
+      </div>
+    );
+  }
+}
